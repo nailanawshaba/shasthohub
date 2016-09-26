e1631a58e8ebf1fd992cd4ac96d5b2ef6fe64b5e
Miles Steele <miles@milessteele.com>
2016-09-26 16:48:37 -0400

    TEMP

---
+++ go/pvl/regs_test.go
@@ -0,0 +1,11 @@
+// Copyright 2016 Keybase, Inc. All rights reserved. Use of
+// this source code is governed by the included BSD license.
+
+package pvl
+
+import "testing"
+
+func TestNamedRegsStore(t *testing.T) {
+	// TODO test that the "" key never works
+	t.Fatalf("TODO write test")
+}
