45e24a7b1a6245c9d06142f60e57ffc944963294
Michał Zochniak <michal@zapu.net>
2017-06-09 19:47:34 +0200

    Fix NoKeyError not getting its Msg through in ImportStatusAsError
    Also be more descriptive when loadSelfKey in `pgp encrypt` fails,
    mention that there is --no-self flag.

--- go/libkb/rpc_exim.go
+++ go/libkb/rpc_exim.go
@@ -242,7 +242,7 @@ func ImportStatusAsError(s *keybase1.Status) error {
 		}
 		return KeyExistsError{fp}
 	case SCKeyNotFound:
-		return NoKeyError{}
+		return NoKeyError{s.Desc}
 	case SCKeyNoEldest:
 		return NoSigChainError{}
 	case SCStreamExists:
